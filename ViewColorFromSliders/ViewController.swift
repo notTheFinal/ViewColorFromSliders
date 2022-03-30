fee0cbc6bf7300de06edf9fe0474461a00dbc9dd
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2022-03-30 19:25:55 +0800

    Сделать логику приложения

--- ViewColorFromSliders/ViewController.swift
+++ ViewColorFromSliders/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorfulView: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        refreshLabels(redSlider.value, greenSlider.value, blueSlider.value)
+        
+        setColorView()
     }
-
-
+    
+    @IBAction func changeSlidersValue(_ sender: UISlider) {
+        setColorView()
+        refreshLabels(redSlider.value, greenSlider.value, blueSlider.value)
+    }
+    
+    func setColorView() {
+        colorfulView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                               green: CGFloat(greenSlider.value),
+                                               blue: CGFloat(blueSlider.value),
+                                               alpha: 1)
+    }
+    func refreshLabels(_ red: Float, _ green: Float, _ blue: Float) {
+        redValueLabel.text = String(format: "%.2f", red)
+        greenValueLabel.text = String(format: "%.2f", green)
+        blueValueLabel.text = String(format: "%.2f", blue)
+    }
 }
-
